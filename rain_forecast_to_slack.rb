2411ddd3c691250279c6df8084cb92e63efba200
kchinda <kchinda@aiming-inc.com>
2017-06-06 13:05:06 +0900

    Add error notification

--- rain_forecast_to_slack.rb
+++ rain_forecast_to_slack.rb
@@ -3,16 +3,39 @@ require 'slack-notifier'
 
 SLACK_ACCESS_TOKEN = ENV['SLACK_INCOMING_WEBHOOK']
 SLACK_NOTIFY_TO = ENV['SLACK_NOTIFY_TO']
+SLACK_ERROR_NOTIFY_TO = ENV['SLACK_ERROR_NOTIFY_TO']
 YAHOO_APP_ID = ENV['YAHOO_APP_ID']
 LOCATION_COORDINATES = ENV['LOCATION_COORDINATES'] # ex) 139.700306,35.689407
 LOCATION_NAME = ENV['LOCATION_NAME'] # 表示用テキスト
 
-rs = RainSensor.new(coordinates: LOCATION_COORDINATES, yahoo_app_id: YAHOO_APP_ID)
-result = rs.result
-exit unless result
+def send_to_slack(access_token, text, opts)
+  notifier = Slack::Notifier.new(access_token)
+  notifier.ping(text, opts)
+end
 
-notifier = Slack::Notifier.new SLACK_ACCESS_TOKEN
-notifier.ping result,
-              username: "#{LOCATION_NAME} の雨模様をお知らせ",
-              channel: SLACK_NOTIFY_TO,
-              icon_emoji: ':rain_cloud:'
+begin
+  rs = RainSensor.new(coordinates: LOCATION_COORDINATES, yahoo_app_id: YAHOO_APP_ID)
+  result = rs.result
+
+  unless result
+    $stdout.puts "It is not raining at #{LOCATION_NAME}."
+    exit
+  end
+
+  $stdout.puts "#{result}\n at #{LOCATION_NAME}."
+  send_to_slack SLACK_ACCESS_TOKEN,
+                result,
+                {
+                  username: "#{LOCATION_NAME} の雨模様をお知らせ",
+                  channel: SLACK_NOTIFY_TO,
+                  icon_emoji: ':rain_cloud:'
+                }
+rescue => e
+  $stderr.puts e
+
+  if SLACK_ERROR_NOTIFY_TO
+    send_to_slack SLACK_ACCESS_TOKEN,
+                  e.inspect,
+                  { channel: SLACK_ERROR_NOTIFY_TO }
+  end
+end
